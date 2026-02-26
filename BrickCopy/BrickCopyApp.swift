6453f84ccf0eefdbd2ab4291014f62d1086b579e
TomasVodak <vodak.tomas2@gmail.com>
2026-02-26 18:24:47 +0100

    Initial Commit

---
+++ BrickCopy/BrickCopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrickCopyApp.swift
+//  BrickCopy
+//
+//  Created by Tomas Vodak on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct BrickCopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
